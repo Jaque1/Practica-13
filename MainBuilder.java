7232db304dcf8a97db120c984dd35f837aa4b9ee
karladaniela5 <karla.daniela550@gmail.com>
2019-11-12 10:27:29 -0600

    Programa

---
+++ MainBuilder.java
@@ -0,0 +1,17 @@
+public class MainBuilder {
+
+	public static void main (String[] args) {
+
+		Persona madre = new Persona.Builder("Maria")
+                .setMunicipio("Selva")
+                .setMayor(37)
+                .setLugarTrabajo("Google")
+                .build();
+
+		Persona hijo = new Persona.Builder("Pedro")
+                .setMenor(4)
+                .setColegio("Colegio de Selva")
+                .build();
+                
+	}
+}
